bf123b3b571ee4fc348fcf501576d6b5f5daf7b9
maurodamatta <maurodamatta00@gmail.com>
2024-04-04 19:38:27 -0300

    Catalog last page

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -6,38 +6,62 @@ import { ProductDTO } from '../../../models/product';
 import { useEffect, useState } from 'react';
 import * as productService from '../../../services/product-service';
 
+type QueryParams = {
+    page: number;
+    name: string;
+}
+
 export default function Catalog() {
+
+    const [isLastPage, setIsLastPage] = useState(false);
 
     const [products, setProducts] = useState<ProductDTO[]>([]);
 
-    const [productName, setProductname] = useState("");
+    const [queryParams, setQueryParams] = useState<QueryParams>({
+        page: 0,
+        name: ""
+    });
 
     useEffect(() => {
-        productService.findPageRequest(0, productName)
+        productService.findPageRequest(queryParams.page, queryParams.name)
             .then(response => {
-                setProducts(response.data.content);      
+                const nextPage = response.data.content;
+                setProducts(products.concat(nextPage));
+                setIsLastPage(response.data.last);
             });
-    }, [productName]);
+    }, [queryParams]);
 
-    function handleSearch(seachText: string) {
-        setProductname(seachText)
+    function handleSearch(searchText: string) {
+        setProducts([]);
+        setQueryParams({ ...queryParams, page: 0, name: searchText });
+    }
+
+    function handleNextPageClick() {
+        setQueryParams({ ...queryParams, page: queryParams.page + 1 });
     }
 
     return (
-            <main>
-                <section id="catalog-section" className="dsc-container">
-                    <SearchBar onSearch={handleSearch} />
+        <main>
+            <section id="catalog-section" className="dsc-container">
+                <SearchBar onSearch={handleSearch} />
 
-                    <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-                        {
-                            products.map(
-                                product => <CatalogCard key={product.id} product={product} />
-                                )
-                        }
+                <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
+                    {
+                        products.map(
+                            product => <CatalogCard key={product.id} product={product} />
+                        )
+                    }
+                </div>
+                {   
+                    !isLastPage &&
+                    <div onClick={handleNextPageClick}>
+                        <ButtonNextPage text='Carregar mais' />
                     </div>
+                }
 
-                    <ButtonNextPage text='Carregar mais'/>
-                </section>
-            </main>
+
+
+            </section>
+        </main>
     );
 }
