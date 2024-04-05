d3c14887e55caa6c75ac9bd857c9f5942c50c495
maurodamatta <maurodamatta00@gmail.com>
2024-04-05 20:41:32 -0300

    Context token payload

--- src/components/HeaderClient/index.tsx
+++ src/components/HeaderClient/index.tsx
@@ -1,23 +1,38 @@
 import './styles.css'
 import { Link } from 'react-router-dom';
 import CartIcon from '../CartIcon';
+import iconAdmin from '../../assets/admin.svg';
+import * as authService from '../../services/auth-service';
+import { useContext } from 'react';
+import { ContextToken } from '../../utils/context-token';
 
 export default function HeaderClient() {
 
+    const { contextTokenPayload } = useContext(ContextToken);
 
     return (
         <header className="dsc-header-client">
             <nav className="dsc-container">
                 <Link to="/">
-                <h1>DSCommerce</h1>
+                    <h1>DSCommerce</h1>
                 </Link>
                 <div className="dsc-navbar-right">
                     <div className="dsc-menu-items-container">
-                    <Link to="/cart">
-                        <div className="dsc-menu-item">
-                            <CartIcon />    
-                        </div>
-                    </Link>
+                        {
+                            contextTokenPayload &&
+                            authService.hasAnyRoles(['ROLE_ADMIN']) &&
+                            <Link to="/admin">
+                                <div className="dsc-menu-item">
+                                    <img src={iconAdmin} alt="Admin" />
+                                </div>
+                            </Link>
+
+                        }
+                        <Link to="/cart">
+                            <div className="dsc-menu-item">
+                                <CartIcon />
+                            </div>
+                        </Link>
                     </div>
                     <Link to="/login">
                         Entrar
