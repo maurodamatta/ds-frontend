b0b6cea966b4a744b62239dea6579e75ef0c1490
maurodamatta <maurodamatta00@gmail.com>
2024-04-05 15:59:40 -0300

    isAuthenticated

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -1,5 +1,13 @@
+export type RoleEnum = "ROLE_ADMIN" | "ROLE_CLIENT"
+
 export type CredentialsDTO = {
     username: string;
     password: string;
-    
 }
+
+export type AccessTokenPayloadDTO = {
+    exp: number,
+    user_name: string,
+    authorities: RoleEnum[];
+}
+
--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,9 +1,10 @@
 import QueryString from "qs";
-import { CredentialsDTO } from "../models/auth";
+import { AccessTokenPayloadDTO, CredentialsDTO } from "../models/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
 import * as accessTokenRepository from '../localstorage/access-token-repository';
+import jwtDecode from "jwt-decode";
 
 export function loginRequest(loginData: CredentialsDTO) {
 
@@ -12,9 +13,9 @@ export function loginRequest(loginData: CredentialsDTO) {
         Authorization: "Basic " + window.btoa(CLIENT_ID + ":" + CLIENT_SECRET)
     }
 
-    const requestBody = QueryString.stringify({...loginData, grant_type: "password"});
+    const requestBody = QueryString.stringify({ ...loginData, grant_type: "password" });
 
-    const config : AxiosRequestConfig = {
+    const config: AxiosRequestConfig = {
         method: "POST",
         url: "/oauth/token",
         data: requestBody,
@@ -34,4 +35,23 @@ export function saveAccessToken(token: string) {
 
 export function getAccessToken() {
     return accessTokenRepository.get();
+}
+
+export function getAccessTokenPayload(): AccessTokenPayloadDTO | undefined {
+    try {
+        const token = accessTokenRepository.get();
+        return token == null
+            ? undefined
+            : (jwtDecode(token) as AccessTokenPayloadDTO);
+    } catch (error) {
+        return undefined;
+    }
+}
+
+export function isAuthenticated(): boolean {
+    let tokenPayload = getAccessTokenPayload();
+    if(tokenPayload && tokenPayload.exp * 1000 > Date.now()){
+        return true;
+    }
+    return false;
 }
