e0e54f66edd1c26bf8ba381d4074f7ee26ff9602
maurodamatta <maurodamatta00@gmail.com>
2024-04-05 12:33:06 -0300

    Admin page layout

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Cart from './routes/ClientHome/Cart';
 import { useState } from 'react';
 import { ContextCartCount } from './utils/context-cart';
 import Login from './routes/ClientHome/Login';
+import Admin from './routes/Admin';
+import AdminHome from './routes/Admin/AdminHome';
 
 
 export default function App() {
@@ -14,7 +16,7 @@ export default function App() {
 
   return (
 
-    <ContextCartCount.Provider value={{contextCartCount, setContextCartCount}}>
+    <ContextCartCount.Provider value={{ contextCartCount, setContextCartCount }}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<ClientHome />}>
@@ -23,6 +25,9 @@ export default function App() {
             <Route path="product-details/:productId" element={<ProductDetails />} />
             <Route path="cart" element={<Cart />} />
             <Route path="login" element={<Login />} />
+          </Route>
+          <Route path="/admin/" element={<Admin />} >
+            <Route index element={<AdminHome />} />
           </Route>
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
